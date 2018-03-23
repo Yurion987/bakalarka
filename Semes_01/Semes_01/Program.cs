1c0b1e8d3e09d086236a0b4dc17445db33242b25
Yurion987 <37672916+Yurion987@users.noreply.github.com>
2018-03-23 15:08:59 +0100

    Dva

--- Semes_01/Semes_01/Program.cs
+++ Semes_01/Semes_01/Program.cs
@@ -16,90 +16,116 @@ namespace Semes_01
 
     class Program
     {
-        List<List<string>> tabulka;
-        HtmlDocument htmlDoc;
+
+        List<List<string>> tabulkaZoSuboru;
         IWebDriver driver;
+        List<string> tabulkaZWebStranky;
         public Program()
         {
             FirefoxOptions fo = new FirefoxOptions();
-           // fo.AddArgument("--headless");
-           driver = new FirefoxDriver(fo);
-            this.tabulka = new List<List<string>> ();
-            string html = System.IO.File.ReadAllText(@"C:\Users\Yurion\source\repos\Semes_01\Semes_01\doch_november.html");
-            this.htmlDoc = new HtmlDocument();
-            this.htmlDoc.LoadHtml(html);
+            fo.AddArgument("--headless");
+            driver = new FirefoxDriver(fo);
+            this.tabulkaZWebStranky = new List<string>();
+            this.tabulkaZoSuboru = new List<List<string>>();
+           
         }
-
-
-
 
         static void Main(string[] args)
         {
 
             Program p = new Program();
-            // ZISKANIE HTML CODU A ULOZENIE DO KONKRETNEHO SUBORU
-
-            //  WebClient wb = new WebClient();
-            // wb.DownloadFile("https://www.arclab.com/en/kb/csharp/download-file-from-internet-to-string-or-file.html", @"C:\Users\Yurion\source\repos\Semes_01\Semes_01\testDownload");
-            //string text = System.IO.File.ReadAllText(@"C:\Users\Yurion\source\repos\Semes_01\Semes_01\doch_november.html");
-
-            //NEVIEM MOZNO AJ FUNGUJE
-
-            // doc.Load(@"C:\Users\Yurion\source\repos\Semes_01\Semes_01\doch_november.html");
-            // var web = new HtmlWeb();
-            // var htmlDoc = new HtmlDocument();
-            //var doc = web.Load("https://www.arclab.com/en/kb/csharp/download-file-from-internet-to-string-or-file.html");
-            // htmlDoc.LoadHtml(text);
-
-           
-
-             //IST NA WEB STRANKU
-             p.driver.Navigate().GoToUrl("https://www.jablonet.net/");
-             p.driver.FindElement(By.Id("login-opener")).Click();
-             p.driver.FindElement(By.Id("login-email")).SendKeys("uhrin9@stud.uniza.sk");
-             p.driver.FindElement(By.Id("login-heslo")).SendKeys("DGbfhk");
-             p.driver.FindElement(By.Id("loginButton")).Click();
-             WebDriverWait wait = new WebDriverWait(p.driver, TimeSpan.FromSeconds(50));
-             p.driver.Navigate().GoToUrl("https://www.jablonet.net/app/ja100?service=257168");
-            
-            wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("more_info_icon")));
-            
-            for (int i = 0; i < 10; i++)
-            {
-                p.driver.FindElement(By.ClassName("more_info_icon")).Click();
-                System.Threading.Thread.Sleep(1000);
-            }
-
-            // p.driver.Quit();
+            p.nacitajStranku();
             p.parsingTable();
-
+            p.rozparsuj_WebStranku();
+            p.driver.Quit();
+            Console.ReadLine();
         }
 
         public void parsingTable()
         {
+            HtmlDocument htmlDoc;
+            string html = System.IO.File.ReadAllText(@"C:\Users\Yurion\source\repos\Semes_01\Semes_01\doch_november.html");
+            htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml(html);
             foreach (HtmlNode row in htmlDoc.DocumentNode.SelectNodes("/html/body/table/tr").Skip(1))
             {
-                tabulka.Add(new List<string>());
+                tabulkaZoSuboru.Add(new List<string>());
                 int stop = 0;
                 foreach (HtmlNode cell in row.SelectNodes("th|td"))
                 {
                     if (stop != 3)
                     {
                         string bunka = cell.InnerHtml;
-                        if (stop == 2) {
-                            bunka = bunka.Substring(9, bunka.Length-9);
-                            string ID = bunka.Substring(0,bunka.IndexOf(":"));
-                            tabulka[tabulka.Count - 1].Add(ID);
+                        if (stop == 2)
+                        {
+                            bunka = bunka.Substring(9, bunka.Length - 9);
+                            string ID = bunka.Substring(0, bunka.IndexOf(":"));
+                            tabulkaZoSuboru[tabulkaZoSuboru.Count - 1].Add(ID);
                             bunka = bunka.Substring(bunka.IndexOf(" "), bunka.Length - bunka.IndexOf(" "));
                         }
-                        tabulka[tabulka.Count - 1].Add(bunka);
-                        
-                        stop++;                
+                        tabulkaZoSuboru[tabulkaZoSuboru.Count - 1].Add(bunka);
+
+                        stop++;
                     }
-                    else {
+                    else
+                    {
                         break;
                     }
                 }
+            }
+        }
+        public void rozparsuj_WebStranku() {
+            //posledny je button
+            var clasSec = driver.FindElements(By.ClassName("section"));
+           
+            for (int i = 0; i < clasSec.Count - 1; i++)
+            {
+                //dostanie datumu zo stranky
+                var datum = clasSec[i].FindElement(By.ClassName("before_day")).Text;
+                var datumMin = clasSec[i].FindElement(By.ClassName("small_day")).Text;
+                foreach (var item in clasSec[i].FindElements(By.Id("timeline")))
+                {
+
+                    foreach (var item2 in item.FindElements(By.ClassName("ON")))
+                    {
+
+                        string meno = item2.FindElement(By.ClassName("name")).Text;
+                        string cas = item2.FindElement(By.ClassName("time")).Text;
+                        if (datumMin != "")
+                        {
+                            tabulkaZWebStranky.Add(datumMin);
+                            tabulkaZWebStranky.Add(cas);
+                            tabulkaZWebStranky.Add(meno);
+                        }
+                        else
+                        {
+                            tabulkaZWebStranky.Add(datum);
+                            tabulkaZWebStranky.Add(cas);
+                            tabulkaZWebStranky.Add(meno);
+                        }
+                    }
+                }
+            }
+            foreach (var item in tabulkaZWebStranky)
+            {
+                Console.WriteLine(item);
+            }
+        }
+        public void nacitajStranku() {
+            int pocetLoadZaznamov = 4;
+
+            driver.Navigate().GoToUrl("https://www.jablonet.net/");
+            driver.FindElement(By.Id("login-opener")).Click();
+            driver.FindElement(By.Id("login-email")).SendKeys("uhrin9@stud.uniza.sk");
+            driver.FindElement(By.Id("login-heslo")).SendKeys("DGbfhk");
+            driver.FindElement(By.Id("loginButton")).Click();
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+            driver.Navigate().GoToUrl("https://www.jablonet.net/app/ja100?service=257168");
+            wait.Until(ExpectedConditions.ElementToBeClickable(By.ClassName("more_info_icon")));
+            for (int i = 0; i < pocetLoadZaznamov; i++)
+            {
+                driver.FindElement(By.ClassName("more_info_icon")).Click();
+                System.Threading.Thread.Sleep(1500);
             }
         }
     }
